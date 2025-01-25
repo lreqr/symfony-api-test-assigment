55cdb2f6cd6964d4714befa98f3a5afaca3ddc32
Vlad <lreqr03@gmail.com>
2025-01-25 15:20:33 +0000

    метод регистрации

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -5,6 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class AuthController extends AbstractController
 {
@@ -14,5 +19,24 @@ final class AuthController extends AbstractController
         return $this->render('auth/index.html.twig', [
             'controller_name' => 'AuthController',
         ]);
+    }
+
+    public function register(Request $request, UserPasswordHasherInterface $hasher, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (empty($data['email']) || empty($data['password'])) {
+            return new JsonResponse(['error' => 'Invalid data'], 400);
+        }
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setPassword($hasher->hashPassword($user, $data['password']));
+        $user->setRoles(['ROLE_USER']);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['status' => 'User registered'], 201);
     }
 }
